6a48e8a3001fc740a2ad5b9bd316f8e5c36b75c9
John Griffiths <gnuchu@gmail.com>
2020-09-28 12:13:22 +0100

    Move search box left

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
@@ -17,6 +18,8 @@
       $('#player_info').DataTable({
         paging: false
       });
+      
+      $('.dataTables_filter').addClass('pull-left');
     });
   </script>
   
